f5b9b8a7c75d952be3ff3e13986dbbc80d9e558b
Anderson (Akame!) <andersonrobertopetry@gmail.com>
2022-12-02 20:55:01 -0300

    scrollReveal

---
+++ main.js
@@ -0,0 +1,15 @@
+//SCROLL REVEAL
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.imgSR', {duration: 3000});
+//ACABA O SCROLL REVEAL
+
+function menuShow() {
+    let menuMobile = document.querySelector('.btn');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "./img/menu.png";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "./img/menu.png";
+    }
+}
