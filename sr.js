0cc605f3bda0cbf84c8edc5ac2cde425bc8ea06f
Anderson (Akame!) <andersonrobertopetry@gmail.com>
2022-12-08 20:34:45 -0300

    finalizando

--- sr.js
+++ sr.js
@@ -1,25 +1,30 @@
 //SCROLL REVEAL
 window.sr = ScrollReveal({reset: true});
-sr.reveal('#srUm', {duration: 2000});
+
+sr.reveal('.divSRCopoComTexto', {
+    origin: 'left',
+    distance: '890px',
+    duration: 2000
+});
+
+sr.reveal('.divCopoComTexto', {
+    origin: 'right',
+    distance: '890px',
+    duration: 2000
+});
 
 sr.reveal('#srUm', {
     rotate: {x: 80, y: 0, z: 0},
     duration: 2000
 }); 
 
-window.sr = ScrollReveal({reset: true});
-sr.reveal('#srDois', {duration: 4000});
-
 sr.reveal('#srDois', {
     rotate: {x: 80, y: 0, z: 0},
-    duration: 2000
+    duration: 4000
 }); 
-
-window.sr = ScrollReveal({reset: true});
-sr.reveal('#srTres', {duration: 6000});
 
 sr.reveal('#srTres', {
     rotate: {x: 80, y: 0, z: 0},
-    duration: 2000
+    duration: 6000
 }); 
 //ACABA O SCROLL REVEAL
